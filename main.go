f252dfe919415ef011120315a8e178b49115c4c1
Edward Carmack <AriemethAcc@gmail.com>
2015-07-15 09:20:24 -0400

    Now using the direct mech.fire method instead of calling the weapon specific fire method

--- main.go
+++ main.go
@@ -16,5 +16,8 @@ func main() {
 	mech1 := mech.NewMech(mech.Weapon{Range: 5, Damage: 1}, "Mech1")
 	mech2 := mech.NewMech(mech.Weapon{Range: 3, Damage: 3}, "Mech2")
 
-	mech1.Weapons.Fire(2, mech2)
+	mech1.Fire(2, mech2)
+	
+//	mech3 := mech.Mech{Weapons:mech.Weapon{Range: 5, Damage: 1}, Name:"testMech3"}
+//	mech3.Fire(mech1)
 }
